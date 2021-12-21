8817d501a5400f23b9c4a1d1db040ea5082fd882
André Vega <ventaslederer@hotmail.com>
2021-12-21 16:08:47 -0600

    Worked on API fetch to get current weather of any city. Also worked on local storage for past searches and the creaions of their buttons to search them again.

--- src/scripts.js
+++ src/scripts.js
@@ -3,20 +3,69 @@ var mainDivEl = $("#mainDiv")
 var resultTodaySectionEl = $("#resultTodaySection")
 var resultForecastSectionEl = $("#resultForecastSection")
 var searchBtnEl = $("#searchBtn")
+var prevCitiesListEl = $("#prevCitiesList")
 
-var cities = ["Atlanta", "San diego", "Los Angeles", "Houston", "Dallas"]
+let prevCitiesArray = []
+
+let inputCityString;
+
 // Search for icons here: https://fontawesome.com/v6.0/icons/cloud-moon?s=solid
 
+const exampleCall = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&units=imperial&appid=acf279683d77cfa942d0855d6b194227"
 
 // Functions
-function createHTML() {
+function storeCity() {
+    prevCitiesString = localStorage.getItem("Previous cities")
+    if (prevCitiesString != null) prevCitiesArray = prevCitiesString.split(",")
+    prevCitiesArray.unshift(inputCityString)
+    prevCitiesArray = new Set(prevCitiesArray)
+    prevCitiesString = Array.from(prevCitiesArray).join(",") 
+    prevCitiesArray = prevCitiesString.split(",")
+    localStorage.setItem('Previous cities', prevCitiesString)
+    renderPrevCities()
+}
+function renderPrevCities() {
+    prevCitiesListEl.html("");
+    for (i = 0; i < prevCitiesArray.length; i++) {
+        const prevCityName = prevCitiesArray[i]
+        const prevCityEl = $("<li>");
+        prevCityEl.html(`
+        <button id="prevCityBtn" class="block text-center text-bold bg-gray-400 w-full rounded-md mb-2 capitalize">${prevCityName} </button>
+        `);
+        prevCitiesListEl.append(prevCityEl);
+    };
+};
+function getWeather(url) {
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data)
+        createHTML(data)
+    })
+}
+function getCoordinates(url) {
+    fetch(url).then(response => response.json()).then(data => {
+        const lat = data[0].lat
+        const lon = data[0].lon
+        const weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=acf279683d77cfa942d0855d6b194227"
+        getWeather(weatherURL)
+    })
+}
+function createHTML(weatherData) {
+    const currentTemp = weatherData.current.temp;
+    const currentWind = weatherData.current.wind_speed;
+    const currentHumid = weatherData.current.humidity;
+    const currentUVI = weatherData.current.uvi;
+    let lvlClass;
+    if (currentUVI < 3) lvlClass = "bg-green-400" 
+    if (currentUVI >= 3 && currentUVI < 6 ) lvlClass = "bg-yellow-400"
+    if (currentUVI >= 6 && currentUVI < 8 ) lvlClass = "bg-orange-400"
+    if (currentUVI >= 8) lvlClass = "bg-red-400"
     const resultTodayHTML = `
-    <h1 class="text-xl font-bold">Atlanta (3/30/2021)</h2>
+    <h1 class="text-xl font-bold capitalize">${inputCityString} *</h2>
     <i class="fa-solid fa-cloud-moon inline-block"></i>
-    <p>Temp: <span>74°F</span></p>
-    <p>Wind: <span>6.67 MPH</span></p>
-    <p>Humidity: <span>46%</span></p>
-    <p>UV index: <span>0.47</span></p>
+    <p>Temp: <span>${currentTemp}°</span></p>
+    <p>Wind: <span>${currentWind} MPH</span></p>
+    <p>Humidity: <span>${currentHumid}%</span></p>
+    <p>UV index: <span class="${lvlClass} rounded-md p-1">${currentUVI}</span></p>
     `
     // const resultForecastHTML = `
     // <h1 class="text-xl font-bold">${city} (3/30/2021)</h2>
@@ -33,7 +82,14 @@ function createHTML() {
     //     resultForecastSectionEl.apppend(divEl)
     // }
 }
+function eventClick() {
+    inputCityString = $(this).siblings("input").val()
+    const geoURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ inputCityString + "&appid=acf279683d77cfa942d0855d6b194227"
+    getCoordinates(geoURL)
+    storeCity()
+    
+}
 // Event listeners
-mainDivEl.on('click', searchBtnEl, function () {
-    createHTML();
-});
+mainDivEl.on('click', "button", eventClick);
+
+//Calls
