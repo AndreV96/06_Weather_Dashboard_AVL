d0fc233fb6dba9451e5da5bdaeb4ff1c4ee91f3f
André Vega <ventaslederer@hotmail.com>
2021-12-23 12:14:33 -0600

    Fixed forecast dates. Correct date displayed now in html.

--- src/scripts.js
+++ src/scripts.js
@@ -1,28 +1,39 @@
 
-var mainDivEl = $("#mainDiv")
-var resultTodaySectionEl = $("#resultTodaySection")
-var resultForecastSectionEl = $("#resultForecastSection")
-var searchBtnEl = $("#searchBtn")
-var prevCitiesListEl = $("#prevCitiesList")
+var mainDivEl = $("#mainDiv");
+var resultTodaySectionEl = $("#resultTodaySection");
+var resultForecastSectionEl = $("#resultForecastSection");
+var searchBtnEl = $("#searchBtn");
+var prevCitiesListEl = $("#prevCitiesList");
 
-let prevCitiesArray = []
-let prevCitiesString = localStorage.getItem("Previous cities")
+let prevCitiesArray = [];
+let prevCitiesString = localStorage.getItem("Previous cities");
 
 let inputCityString;
 
+// Moment variables
+var moment = moment();
+var currentDateG = moment.format('l');
+const days = []
+for (i = 0; i < 5; i++) {
+    const day = moment.add(1, 'days').format('l')
+    days.push(day)
+    console.log(days)
+}
+// const tomorrow = moment.add(1, 'days').format('l')
+// // const afterTomorrow = moment.add(1, 'days').format('l')
+// console.log(tomorrow)
+// console.log(afterTomorrow)
+
 // Search for icons here: https://fontawesome.com/v6.0/icons/cloud-moon?s=solid
 
-const exampleCall = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&units=imperial&appid=acf279683d77cfa942d0855d6b194227"
+const exampleCall = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&units=imperial&appid=acf279683d77cfa942d0855d6b194227";
 
 // Functions
 function storeCity() {
-    console.log(prevCitiesArray)
     prevCitiesArray.unshift(inputCityString);
-    prevCitiesArray = Array.from(new Set(prevCitiesArray)) ;
-    console.log(prevCitiesArray)
+    prevCitiesArray = Array.from(new Set(prevCitiesArray));
     if (prevCitiesArray.length >= 9) prevCitiesArray.pop();
     prevCitiesString = prevCitiesArray.join(",");
-    // prevCitiesArray = prevCitiesString.split(",");
     localStorage.setItem('Previous cities', prevCitiesString);
     renderPrevCities();
 }
@@ -30,7 +41,7 @@ function renderPrevCities() {
     prevCitiesListEl.html("");
 
     for (i = 0; i < prevCitiesArray.length; i++) {
-        const prevCityName = prevCitiesArray[i]
+        const prevCityName = prevCitiesArray[i];
         const prevCityEl = $("<li>");
         prevCityEl.html(`
         <button id="prevCityBtn" class="block text-center text-bold bg-gray-400 w-full rounded-md mb-2 capitalize">${prevCityName}</button>
@@ -41,69 +52,76 @@ function renderPrevCities() {
 function getWeather(url) {
     fetch(url).then(response => response.json()).then(data => {
         createHTML(data)
-    })
-}
+    });
+};
 function getCoordinates(url) {
     fetch(url).then(response => response.json()).then(data => {
-        const lat = data[0].lat
-        const lon = data[0].lon
+        const lat = data[0].lat;
+        const lon = data[0].lon;
         const weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=acf279683d77cfa942d0855d6b194227"
-        getWeather(weatherURL)
-    })
-}
+        getWeather(weatherURL);
+    });
+};
 function createHTML(weatherData) {
-    const currentTemp = weatherData.current.temp;
-    const currentWind = weatherData.current.wind_speed;
-    const currentHumid = weatherData.current.humidity;
-    const currentUVI = weatherData.current.uvi;
+    resultForecastSectionEl.html("")
+    const currentData = weatherData.current;
+    const currentTemp = currentData.temp;
+    const currentWind = currentData.wind_speed;
+    const currentHumid = currentData.humidity;
+    const currentUVI = currentData.uvi;
+
     let lvlClass;
-    if (currentUVI < 3) lvlClass = "bg-green-400" 
-    if (currentUVI >= 3 && currentUVI < 6 ) lvlClass = "bg-yellow-400"
-    if (currentUVI >= 6 && currentUVI < 8 ) lvlClass = "bg-orange-400"
-    if (currentUVI >= 8) lvlClass = "bg-red-400"
+    if (currentUVI < 3) lvlClass = "bg-green-400";
+    if (currentUVI >= 3 && currentUVI < 6 ) lvlClass = "bg-yellow-400";
+    if (currentUVI >= 6 && currentUVI < 8 ) lvlClass = "bg-orange-400";
+    if (currentUVI >= 8) lvlClass = "bg-red-400";
     const resultTodayHTML = `
-    <h1 class="text-xl font-bold capitalize">${inputCityString} *</h2>
+    <h1 class="text-xl font-bold capitalize">${inputCityString} ${currentDateG}</h2>
     <i class="fa-solid fa-cloud-moon inline-block"></i>
     <p>Temp: <span>${currentTemp}°</span></p>
     <p>Wind: <span>${currentWind} MPH</span></p>
     <p>Humidity: <span>${currentHumid}%</span></p>
     <p>UV index: <span class="${lvlClass} rounded-md p-1">${currentUVI}</span></p>
     `
-    // const resultForecastHTML = `
-    // <h1 class="text-xl font-bold">${city} (3/30/2021)</h2>
-    // <i class="fa-solid fa-cloud-moon inline-block"></i>
-    // <p>Temp: <span>74°F</span></p>
-    // <p>Wind: <span>6.67 MPH</span></p>
-    // <p>Humidity: <span>46%</span></p>
-    // `
-    resultTodaySectionEl.html(resultTodayHTML)
-    // for (i = 0; i < cities.length; i++){
-    //     city = cities[i]
-    //     const divEl = document.createElement("div");
-    //     divEl.html(resultForecastHTML)
-    //     resultForecastSectionEl.apppend(divEl)
-    // }
-}
-function inputSearch() {
-    inputCityString = $(this).siblings("input").val()
-    const geoURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ inputCityString + "&appid=acf279683d77cfa942d0855d6b194227"
-    getCoordinates(geoURL)
-    storeCity()
+    resultTodaySectionEl.html(resultTodayHTML);
+    for (i = 1; i < 6; i++) {        
+    let resultForecastEl = document.createElement("div");
+    let dailyData = weatherData.daily[i]
+    let dailyTemp = dailyData.temp.day;
+    let dailyWind = dailyData.wind_speed;
+    let dailyHumid = dailyData.humidity;
     
-}
+    resultForecastEl.innerHTML = `
+    <h1 class="text-xl font-bold capitalize">${inputCityString} ${days[i-1]}</h2>
+    <i class="fa-solid fa-cloud-moon inline-block"></i>
+    <p>Temp: <span>${dailyTemp}</span></p>
+    <p>Wind: <span>${dailyWind} MPH</span></p>
+    <p>Humidity: <span>${dailyHumid}</span></p>
+    `
+    resultForecastSectionEl.append(resultForecastEl);
+    };
+};
+function inputSearch() {
+    const inputEl = $(this).siblings("input");
+    inputCityString = inputEl.val();
+    inputEl.val("");
+    searchEvent(inputCityString);
+};
 function prevCitiesSearch() {
     inputCityString = $(this).text();
-    console.log(inputCityString)
+    searchEvent(inputCityString);
+};
+function searchEvent(inputCityString) {
     const geoURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ inputCityString + "&appid=acf279683d77cfa942d0855d6b194227"
-    getCoordinates(geoURL)
-    storeCity()
-}
+    getCoordinates(geoURL);
+    storeCity();
+};
 // Event listeners
 mainDivEl.on('click', "#searchBtn", inputSearch);
 mainDivEl.on('click', "#prevCityBtn", prevCitiesSearch);
 
 //Calls
 if (prevCitiesString != null) {
-    prevCitiesArray = prevCitiesString.split(",")
+    prevCitiesArray = prevCitiesString.split(",");
     renderPrevCities();
-} 
+};
